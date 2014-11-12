64162d01f887cfc6f2f7b76c6fc7f7af91033364
FakeKane <andrewyli@gmail.com>
2014-11-11 20:49:48 -0500

    file input added

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// Example story: He was so [adjective] that he [verb]ed John in the face, sending him into a/an [noun].
+
+
+use std::io::File;
+
+fn main {
+    let story = File::open(&Path::new("story.txt")).read_to_end();
+    
+}
