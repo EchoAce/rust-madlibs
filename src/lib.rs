4e3cca6d10ff26edb3674eede8d744db2bc301e7
FakeKane <andrewyli@gmail.com>
2014-11-12 11:27:43 -0500

    moved function to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-#[test]
-fn it_works() {
+use std::io;
+
+pub fn read_input() -> String {
+    let mut story = "";
+    println!("Please enter a line of the story. Enter a blank line to stop");
+    for line in io::stdin().lines() {
+        match line.to_string().as_slice() {
+            "Ok(\n)" => break,
+            _ => {
+                println!("Please enter a line of the story. Enter a blank line to stop");
+                //story = concat!(story, _);
+            },
+        }
+    }
+    story.to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 // Example story: He was so [adjective] that he [verb]ed John in the face,
 // sending him into a/an [noun].
 
-use std::io;
+extern crate "rust-madlibs" as madlibs;
+use madlibs::read_input;
 
 fn main() {
-    let mut story = "";
-    println!("Please enter a line of the story. Enter a blank line to stop");
-    for line in io::stdin().lines() {
-        match line.to_string().as_slice() {
-            "Ok(\n)" => break,
-            _ => {
-                println!("Please enter a line of the story. Enter a blank line to stop");
-                //story = concat!(story, _);
-            },
-        }
-    }
+    let akiva = read_input();
+    println!("{}", akiva);
 }
